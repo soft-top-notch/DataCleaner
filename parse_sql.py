bcf08cf1f630c63b7973912eb550fae1e5a30077
Paul Howell <paul.howell@gmail.com>
2018-01-31 01:06:11 -0500

    Removed csv writer, wrote character parser

--- parse_sql.py
+++ parse_sql.py
@@ -3,7 +3,6 @@
 
 Should be imported and used within datacleaner.py
 """
-import csv
 import re
 import sys
 
@@ -11,14 +10,39 @@ INSERT_RE = re.compile('^INSERT INTO `?\w+`? \(((?:`[\w\s]+`,?\s*)+)\) VALUES')
 VALUES_RE = re.compile('\(((?:\'?[^\'`]*\'?,?)+)\)')
 
 
+def clean_line(line):
+    """Removes quotes while perserving included special characters.
+
+    Returns sanitized list of elements.
+    """
+    data = []
+    element = ''
+    quotes = []
+    for char in line:
+        if char in "'":
+            if quotes:
+                if char == quotes[-1]:
+                    quotes.pop()
+                else:
+                    quotes.append(char)
+            else:
+                quotes.append(char)
+        elif char in ' ' and not element:
+            pass
+        elif char == ',' and not quotes:
+            data.append(element)
+            element = ''
+        else:
+            element += char
+    return data
+
+
 def parse_sql(filename):
     """Opens sql file and parses INSERT statements, outputing data into CSV."""
     fields = []
     end_pattern = []
     data_count = 0
-    with open(filename + '.csv', 'wb') as csvfile:
-        csvwriter = csv.writer(
-            csvfile, lineterminator='\n', quoting=csv.QUOTE_ALL, strict=True)
+    with open(filename + '.csv', 'w') as csvfile:
         with open(filename) as sqlfile:
             for line in sqlfile:
                 values = []
@@ -28,19 +52,18 @@ def parse_sql(filename):
                         if not fields:
                             fields = re.sub('\`', '',
                                             insert_match.group(1)).split(',')
-                            csvwriter.writerow(fields)
+                            csvfile.write(','.join('"{0}"'.format(field)
+                                                   for field in fields) + '\n')
                         end_pattern.append(');')
                 if end_pattern:
                     values_match = VALUES_RE.search(line)
                     if values_match:
-                        values_no_quotes = re.sub('[\'\"]', '',
-                                                  values_match.group(1))
-                        values = re.sub(',\s+', ',',
-                                        values_no_quotes).split(',')
+                        values = clean_line(values_match.group(1))
                     if end_pattern[-1] in line:
                         end_pattern.pop()
                 if values:
-                    csvwriter.writerow(values)
+                    csvfile.write(','.join('"{0}"'.format(value)
+                                           for value in values) + '\n')
                     data_count += 1
     # Return the number of fields and number of lines of data
     return len(fields), data_count
