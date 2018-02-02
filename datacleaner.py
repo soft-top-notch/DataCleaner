d53dae0ed33184b5cd897bfc6789811b6a3ba1a3
Mustafa Başer <mbaser@mail.com>
2018-02-02 14:21:37 +0300

    add csv sniffer to guess delimiter and some code enhancements

--- datacleaner.py
+++ datacleaner.py
@@ -1,9 +1,138 @@
-import sys, shutil
-import os
+import os, sys, shutil
+import csv, codecs, cStringIO
+
 
 if not len(sys.argv) > 1:
     print "Usage: {} path_to_file.csv".format(sys.argv[0]) 
     sys.exit(0)
+
+delims = ('\t', ' ', ';', ':', ',', '|')
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
+class myDialect(csv.Dialect):
+    delimiter = ','
+    quotechar = '"'
+    doublequote = True
+    skipinitialspace = False
+    lineterminator = '\n'
+    quoting = csv.QUOTE_ALL
+    escapechar='\\'
+
+
+def find_mode(L):
+    mDict = {}
+    for i in L:
+        if i in mDict: 
+            mDict[i] +=1
+        else:
+            mDict[i] =1
+
+    mList = [ (mDict[i],i) for i in mDict ]
+
+    return max(mList)
+
+
+def find_column_count(f, dialect, iter_n = 500):
+    i=1
+    L=[]
+    reader = UnicodeReader(f, dialect=dialect)
+    for l in reader:
+        if l:
+            while True:
+                if not l:
+                    break
+                if not l[-1]:
+                    l.pop()
+                else:
+                    break
+            if l:
+                L.append(len(l))
+        if i >= iter_n:
+            break
+        i +=1
+
+    mode = find_mode(L)
+    return mode[1]
+
+
+def guess_delimeter_by_csv(F):
+    F.seek(0)
+
+    sniffer = csv.Sniffer()
+
+    try:
+        dialect = sniffer.sniff(F.read(1024*5), delimiters = delims)
+
+        if not dialect.escapechar:
+            dialect.escapechar='\\'
+    
+        F.seek(0)
+
+        column_count = find_column_count(F, dialect)
+        
+        return dialect, column_count
+    except:
+        return None
+
 
 
 def ask_user_for_delimeter():
@@ -11,89 +140,7 @@ def ask_user_for_delimeter():
     csv_delimeter = raw_input("Please idetify delimeter to be used for parsing csv file: ")
     csv_column_count = raw_input("Please identify column number: ")
 
-    return csv_delimeter, csv_column_count
-
-def x_guess_delimeter(F):
-
-    print "Guessing delimeter by looking first 100 lines"
-
-    delim_counts = {'\t':[],
-                ' ': [],
-                ';': [],
-                ':': [],
-                ',': [],
-                '|': [],
-                }
-
-    csv_delimeter = None
-    csv_column_count = 0
-
-    c=0
-    for l in F:
-        if c>=100:
-            break
-        ls=l.strip()
-        if ls:
-            for d in delim_counts:
-                dc = ls.count(d)
-                if not dc in delim_counts[d]:
-                    delim_counts[d].append(ls.count(d))
-            c +=1
-
-
-    delimeter_list = []
-
-    for d in delim_counts:
-        if len(delim_counts[d]) == 1:
-            if not delim_counts[d][0] == 0:
-                
-                delim = (d, delim_counts[d][0])
-                if not delim in delimeter_list:
-                    delimeter_list.append(delim)
-
-    print "Here is the first 10 lines\n"
-    print "-"*30
-    F.seek(0)
-    
-    c=0
-    
-    for l in F:
-        print l.strip()
-        if c>=10:
-            break
-        c +=1
-
-    print "-"*30
-    print
-
-    if not delimeter_list:
-        print "Can't guess delimeter"
-
-        csv_delimeter, csv_column_count = ask_user_for_delimeter()
-
-    elif  len (delimeter_list)> 1:
-        print "Can't guess delimeter, found multiple delimeters"
-        for d in delimeter_list:
-            if d[0] == '\t':
-                print "Guessed delimeter: {tab}"
-            else:
-                print "Guessed delimeter:", d[0]
-            print "Guessed column number", d[0]+1
-        
-        csv_delimeter, csv_column_count = ask_user_for_delimeter()
-
-    else:
-        print "Gusessed delimeter -> {}".format('{tab}' if  delimeter_list[0][0] =='\t' else delimeter_list[0][0])
-        print "Guessed column number", delimeter_list[0][1]+1
-
-        r = raw_input("Do you want to proceed with these guessed values? [Y|n]: ")
-        if (not r) or (r in ('Y', 'y')):
-            csv_delimeter = delimeter_list[0][0]
-            csv_column_count = delimeter_list[0][1]+1
-        else:
-            csv_delimeter, csv_column_count = ask_user_for_delimeter()
-            
-    return csv_delimeter, csv_column_count
+    return csv_delimeter, int(csv_column_count)
 
 
 def strip_delimeter(ls, csv_delimeter):
@@ -119,46 +166,57 @@ def strip_delimeter(ls, csv_delimeter):
 
 def guess_delimeter(F):
 
-    delims = ('\t', ' ', ';', ':', ',', '|')
-
-    delim_counts_list={}
-    delim_freq = {}
-    for d in delims:
-        delim_counts_list[d]=[]
-        delim_freq[d] = {}
-
-    x=0
-
-    F.seek(0)
-
-    for l in F:
-        if x>=1000:
-            break
+    
+    csv_guess = guess_delimeter_by_csv(F)
+    
+    if csv_guess:
+        rdialect, csv_column_count = csv_guess
+        csv_delimeter = rdialect.delimiter
+        print ">>> Guess method: CSV Sniffer\n"
+    else:
+        delim_counts_list={}
+        delim_freq = {}
         for d in delims:
-            ls = l.strip()
-            ls = strip_delimeter(ls, d)
-            cnt = ls.count(d)
-            delim_counts_list[d].append(cnt)
-            x+=1
+            delim_counts_list[d]=[]
+            delim_freq[d] = {}
 
-    most_frequent = (None, 0, 0)
+        x=0
 
-    for d in delims:
-        for c in delim_counts_list[d]:
-            if c:
-                if c in delim_freq[d]:
-                    delim_freq[d][c] +=1
-                else:
-                    delim_freq[d][c] = 1
+        F.seek(0)
+
+        for l in F:
+            if x>=1000:
+                break
+            for d in delims:
+                ls = l.strip()
+                ls = strip_delimeter(ls, d)
+                cnt = ls.count(d)
+                delim_counts_list[d].append(cnt)
+                x+=1
+
+        most_frequent = (None, 0, 0)
+
+        for d in delims:
+            for c in delim_counts_list[d]:
+                if c:
+                    if c in delim_freq[d]:
+                        delim_freq[d][c] +=1
+                    else:
+                        delim_freq[d][c] = 1
+                    
+        for d in delim_freq:
+            for c in delim_freq[d]:
                 
-    for d in delim_freq:
-        for c in delim_freq[d]:
-            
-            if delim_freq[d][c] > most_frequent[1]:
-                most_frequent = (d,  delim_freq[d][c], c)
+                if delim_freq[d][c] > most_frequent[1]:
+                    most_frequent = (d,  delim_freq[d][c], c)
 
-    csv_delimeter = most_frequent[0]
-    csv_column_count = most_frequent[2] + 1
+        csv_delimeter = most_frequent[0]
+        csv_column_count = most_frequent[2] + 1
+
+        rdialect = csv.excel
+        rdialect.delimiter = csv_delimeter
+
+        print ">>> Guess method: Custom\n"
 
     print "Here is the first 10 lines\n"
     print "-"*30
@@ -180,11 +238,14 @@ def guess_delimeter(F):
 
     r = raw_input("Do you want to proceed with these guessed values? [Y|n]: ")
     if (not r) or (r in ('Y', 'y')):
-        return csv_delimeter, csv_column_count
-
+        return rdialect, csv_column_count
     else:
         csv_delimeter, csv_column_count = ask_user_for_delimeter()
-        return csv_delimeter, csv_column_count
+        
+        rdialect = csv.excel
+        rdialect.delimiter = csv_delimeter
+
+        return rdialect, csv_column_count
 
 
 def clean(e):
@@ -247,40 +308,46 @@ def parse_file(tfile):
 
         F = open(gc_file,'rb')
 
-        csv_delimeter, csv_column_count = guess_delimeter(F)
-
-        c=0
+        dialect, csv_column_count = guess_delimeter(F)
 
         F.seek(0)
 
-        out_file_csv_name = f_name+'_parsed.csv'
+        out_file_csv_name = f_name+'_cleaned.csv'
         out_file_err_name = f_name+'_error.csv'
         
-        write_delimeter = ','
         
         out_file_csv_file = open(out_file_csv_name+'~','wb')
         
         out_file_err_file = open(out_file_err_name+'~','wb')
-        
-        for li in F:
-            ls = li.strip()
-            if ls:
-                ls = strip_delimeter(ls, csv_delimeter)
-                l = ls.split(csv_delimeter)
-                if l:
-                    if not l[-1]:
-                        l.pop()
 
-                    lc = clean_fields(l)
-                    lc = wrap_fields(lc)
 
-                    if len(l)== csv_column_count:
-                        out_file_csv_file.write(write_delimeter.join(lc)+'\n')
-                    else:
-                        out_file_err_file.write(write_delimeter.join(l)+'\n')
+        orig_reader = UnicodeReader(F, dialect=dialect)
+        clean_writer = UnicodeWriter(out_file_csv_file, dialect=myDialect)
+        error_writer = UnicodeWriter(out_file_err_file, dialect=dialect)
+
+        for l in orig_reader:
+
             
-        print "Output file", out_file_csv_name, "were written"
-        print "Error file", out_file_err_name, "were written"
+            while True:
+                if not l:
+                    break
+                if not l[-1]:
+                    l.pop()
+                else:
+                    break
+            
+            if len(l) == csv_column_count:
+                clean_writer.writerow(l)
+            else:
+                error_writer.writerow(l)
+
+        F.close()
+        out_file_csv_file.close()
+        out_file_err_file.close()
+
+
+        print "Output file", out_file_csv_name+'~', "were written"
+        print "Error file", out_file_err_name+'~', "were written"
 
         print "Removing", gc_file
         os.remove(gc_file)
@@ -298,7 +365,7 @@ def parse_file(tfile):
 
         os.rename(out_file_csv_name+'~', out_file_csv_name)
 
-
+        
 if __name__ == '__main__':
 
     ppath = sys.argv[1]
@@ -311,6 +378,5 @@ if __name__ == '__main__':
                     parse_file(tf)
 
     elif os.path.isfile(ppath):
-        print "parsing"
         parse_file(ppath)
 
