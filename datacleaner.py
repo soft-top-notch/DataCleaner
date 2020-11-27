ca3ea0d5c0215cb40c4896cd6be7d371ca364bad
root <vinny@nightlionsecurity.com>
2020-11-27 05:02:14 -0500

    more checks

--- datacleaner.py
+++ datacleaner.py
@@ -63,8 +63,8 @@ HEX_IP_PATTERN = re.compile(r'UNHEX\(\'(.*?)\'\)')
 # Parts of filename not to include in release name
 UNWANTED_RELEASE = set(UNWANTED)
 UNWANTED_RELEASE.update({
-    '_part1', '_part2', '_part3', '_2017', '_cleaned', '_error', '_vb-2017', '_parsed', '_bcrypt',
-    '[NOHASH]', '[HASH]', 'notfound', 'decrypted', '_notfound', 'nohash',
+    '_part1', '_part2', '_part3', '_2017', '_cleaned', '_error', '_vb-2017', '_parsed', '_bcrypt', '_vB', '_md5', '_emailpass',
+    '[NOHASH]', '[HASH]', 'notfound', 'decrypted', '_notfound', 'nohash', '_dump', 'dump', '.sql', '2018', '2019',
     '-vb-2016', '_vb___17', '_vb___16', '_p2', '_p3', '_p4', '-2016', '-2017', '.sql', '_md5crypt', 'bcrypt'
 })
 
