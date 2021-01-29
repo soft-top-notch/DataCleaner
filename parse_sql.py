a635a7611e54d9091e9da0709ffc367af3cbba11
nunofragoso <nunofragoso67@gmail.com>
2021-01-29 10:34:40 +0800

    parse sql: remove bad characters

--- parse_sql.py
+++ parse_sql.py
@@ -315,6 +315,7 @@ def read_file(sqlfile):
 
         for line in lines:
             line = line.replace('\11\12\15\40-\176', '')
+            line = re.sub(r'\\\\\\"(\w*)\\\\\\"', r'\1', line)
             byte_num += len(line)
             valid_insert = None
 
